d1bc155f48d2a4eeac46646d598e885f30c1e073
Halan Kiprotich <kiprotichhalan@gmail.com>
2026-01-19 11:37:11 +0300

    Initial commit

---
+++ TraversingArray.java
@@ -0,0 +1,10 @@
+public class TraversingArray {
+    public static void main(String[] args) {
+
+        int[] arr ={5, 10, 15, 20};
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+
+        }
+    }
+}
