ffc2f93337120e2bc4b2770f29464d9205835543
Halan Kiprotich <kiprotichhalan@gmail.com>
2026-01-20 10:50:05 +0300

    Initiak commit

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr={4,3,2,6,2,9,11,67};
+        int target=77;
+
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<high){
+            int mid=(low+high)/2;
+
+            if (arr[mid]==target){
+                System.out.println("Elenet found at index:" +mid);
+                return;
+            }
+            else if (arr[mid]<target){
+                low=mid+1;
+
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        System.out.println("Element not found");
+    }
+}
